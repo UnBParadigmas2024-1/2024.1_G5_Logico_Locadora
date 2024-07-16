2be34a9ab53953d15f1eccc8de4509b436bdeca8
pedrohelias <pedrohelias95@hotmail.com>
2024-07-16 15:04:20 -0300

    codigo de recomendacao atualizado
    Co-authored-by: ian-dcg <ianndcg@gmail.com>
    Co-authored-by: sebazac332 <211006957@aluno.unb.br>

--- provisorioLocadora.pl
+++ provisorioLocadora.pl
@@ -4,21 +4,59 @@ cliente(1, 'Eduardo', '111.222.333-44', drama, 100.0).
 cliente(2, 'Pessoa 2', '555.666.777-88', crime, 50.0).
 cliente(3, 'Pessoa 3', '999.888.777-66', action, 75.0).
 cliente(4, 'Eduardo G', '012.821.092-19', adventure, 200.0).
-cliente(5, 'Eduardo1', '077.000.000-99', western, 30.0).
+cliente(5, 'Eduardo1', '077.000.000-99', mystery, 30.0).
 
 % Definindo a base de dados de filmes
-% filme(Codigo, Titulo, Ano, Nota, Genero, Valor, Disponivel).
-filme(1, 'The Shawshank Redemption', 1994, 9.3, drama, 18.4, true).
-filme(2, 'The Godfather', 1972, 9.2, crime, 6.3, true).
-filme(3, 'The Dark Knight', 2008, 9.0, action, 19.8, true).
-filme(4, 'The Godfather Part II', 1974, 9.0, crime, 20.0, true).
-filme(5, '12 Angry Men', 1957, 9.0, crime, 19.6, true).
-filme(6, 'Schindler''s List', 1993, 9.0, drama, 15.0, true).
-filme(7, 'The Lord of the Rings: The Return of the King', 2003, 9.0, action, 7.8, true).
-filme(8, 'Pulp Fiction', 1994, 8.9, crime, 16.4, true).
-filme(9, 'The Lord of the Rings: The Fellowship of the Ring', 2001, 8.8, action, 8.8, true).
-filme(10, 'The Good, the Bad and the Ugly', 1966, 8.8, western, 14.2, true).
 
+filme(237, 'Rebecca', 1940, 8.1, [drama,film-noir,mystery], 19.7, true).
+filme(238, 'Cool Hand Luke', 1967, 8.1, [crime,drama], 10.4, true).
+filme(239, 'The Handmaiden', 2016, 8.1, [drama,romance,thriller], 12.0, true).
+filme(240, 'The 400 Blows', 1959, 8.1, [crime,drama], 18.2, true).
+filme(241, 'The Sound of Music', 1965, 8.1, [biography,drama,family], 13.4, true).
+filme(242, 'It Happened One Night', 1934, 8.1, [comedy,romance], 7.4, true).
+filme(243, 'Persona', 1966, 8.1, [drama,thriller], 4.0, true).
+filme(244, 'Life of Brian', 1979, 8.0, [comedy], 15.7, true).
+filme(245, 'The Iron Giant', 1999, 8.1, [animation,action,adventure], 14.6, true).
+filme(246, 'The Help', 2011, 8.1, [drama], 12.9, true).
+filme(247, 'Dersu Uzala', 1975, 8.2, [adventure,biography,drama], 10.2, true).
+filme(248, 'Aladdin', 1992, 8.0, [animation,adventure,comedy], 13.7, true).
+filme(249, 'Gandhi', 1982, 8.0, [biography,drama,history], 7.2, true).
+
+
+%Processo de recomendação de filme
+
+
+% Função para listar todos os clientes com seus códigos e nomes, vai ser utilizada no processo de recomendação de filme -> o operador do sistema vai escolher qual cliente deseja e filmes recomendados vão aparecer para ele
+listar_clientes :-
+    findall((Codigo, Nome), cliente(Codigo, Nome, _, _, _), Clientes),
+    writeln('Clientes cadastrados:'),
+    listar(Clientes).
+
+% auxliar para listar os clientes cadastrados
+listar([]).
+listar([(Codigo, Nome)|T]) :-
+    format('Código: ~w, Nome: ~w~n', [Codigo, Nome]),
+    listar(T).
+
+
+% recomendador de filmes para ser utilizado dentro da rotina do menu
+recomendar_filmes(GeneroFavorito, FilmesRecomendados) :-
+    findall((Codigo, Titulo),
+            (filme(Codigo, Titulo, _, _, Generos, _, true),
+             member(GeneroFavorito, Generos)),
+            FilmesRecomendados).
+
+
+% Recomendador pra ser chamado no menu. 
+menu_recomendacao :-
+    listar_clientes,
+    writeln('Escolha um cliente pelo código:'),
+    read(Codigo),
+    (   cliente(Codigo, Nome, _, GeneroFavorito, _) ->  recomendar_filmes(GeneroFavorito, FilmesRecomendados),
+        (   FilmesRecomendados \= [] ->  format('Recomendações para ~w: ~n', [Nome]),
+            listar(FilmesRecomendados);   
+            writeln('Sem recomendações'));   
+            writeln('Cliente não encontrado.'), menu_recomendacao).
 
 % % Processo de locação de filme
 % % precisa do codigo dos filmes, somar o valor dos filmes, descontar da carteira do cliente, se tiver fundos? beleza. Se não tiver, reportar que o saldo é insuficiente
@@ -35,31 +73,4 @@ filme(10, 'The Good, the Bad and the Ugly', 1966, 8.8, western, 14.2, true).
 %     filme(Cod, _, _, _, _, Valor, true),
 %     soma_valores(Codigos, TotalRestante),
 %     Total is Valor + TotalRestante.
-
-
-% Função para listar todos os clientes com seus códigos e nomes, vai ser utilizada no processo de recomendação de filme -> o operador do sistema vai escolher qual cliente deseja e filmes recomendados vão aparecer para ele
-listar_clientes :-
-    findall((Codigo, Nome), cliente(Codigo, Nome, _, _, _), Clientes),
-    writeln('Clientes cadastrados:'),
-    listar(Clientes).
-
-%auxiliar usada para listar os clientes no formato específico de apresentação. Vai apresentar Registro e nome do cliente, sendo que a escolha do cliente será feita pelo código do cliente.(registro)
-listar([]).
-listar([(Codigo, Nome)|T]) :-
-    format('Registro: ~w, Nome: ~w~n', [Codigo, Nome]),
-    listar(T). %chamada recursiva para printar a lista
-
-% recomendador
-recomendar_filmes(Nome, FilmesRecomendados) :-
-    cliente(_, Nome, _, GeneroFavorito, _),
-    findall(Titulo, (filme(_, Titulo, _, _, GeneroFavorito, _, true)), FilmesRecomendados).
-
-% seleciona cliente pelo codigo e recomenda filme, ainda precisa rever a questao de todos filmes possíveis ou os ordenados
-menu_recomendacao :-
-    listar_clientes,
-    writeln('Escolha um cliente pelo código:'),
-    read(Codigo),(   cliente(Codigo, Nome, _, GeneroFavorito, _) ->  recomendar_filmes(Nome, FilmesRecomendados), 
-        format('Recomendações para ~w: ~w~n', [Nome, FilmesRecomendados]);
-        writeln('Cliente não encontrado.'),
-        escolher_cliente).
 
